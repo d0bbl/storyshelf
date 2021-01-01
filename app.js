b696283d75b8e5d7bb12acb445a29a6313667f07
dobblexg <kennybajomo@gmail.com>
2021-01-01 19:15:11 +0100

    irrelevant

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const authRoute = require("./routes/authRoute");
+const dBURI = require("./config/keys");
 
 const app = express();
 
-const port = process.env.PORT || 5000;
+const port = 5000;
 app.listen(port, () => {
 console.log("listening for requests");
 });
@@ -11,3 +13,5 @@ console.log("listening for requests");
 app.get("/", (req, res) => {
   res.send("It works");
 });
+
+// app.use("/auth", authRoute);
---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoURI:"mongodb+srv://romeo:juliet@storybooks.thzje.mongodb.net/romeo?retryWrites=true&w=majority",
+  googleClientID:"216808835109-tl20s4rjq50unjok42ln3g0bbb9ciuh5.apps.googleusercontent.com",
+  googleClientSecret:"IugpX5WpZkpuXWhkgUio8ryb"
+ }
